2d5db1e1322ffcaf891b9a33f535ac6063af3cfc
colorocker <colorocker@gmail.com>
2020-09-20 12:38:13 -0300

    Add Anim to the project

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    PlayerController movement;    //Reference to the PlayerMovement script component
+    Rigidbody2D rigidBody;
+
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
